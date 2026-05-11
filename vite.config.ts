c80b835d70549cfebef42bd943d5be2f8b33ffec
Samuel <Castillo>
2026-05-11 13:24:46 -0600

    correccion build para vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/app/',
   build: {
-    outDir: '../api/static/app',
+    outDir: '../chordsApi/static/app',
     emptyOutDir: true,
   },
 })
